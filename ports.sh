95623a69cc10d9c084d57030d2388ac938c9d1c3
N3EZIO <60109048+N3EZIO@users.noreply.github.com>
2020-05-08 18:18:08 +0000

    Update ports.sh

--- ports.sh
+++ ports.sh
@@ -3,5 +3,5 @@
 if [ "$#" -ne 1 ]; then
     echo "Syntax: ./ports.sh <ip>"
 else
-nmap $1| grep open | cut -d " " -f 1 | cut -d"/" -f 1 | tr '\n' ' '
+nmap $1| grep open | cut -d " " -f 1 | cut -d"/" -f 1 | tr '\n' ','
 fi
